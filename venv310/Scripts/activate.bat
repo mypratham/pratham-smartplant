c3306b63a2c6cdc7a42f18c5fe46cbb48cc8003e
mypratham <info.prathamservicesllp@gmail.com>
2026-09-09 11:24:52 +0530

    A

--- venv310/Scripts/activate.bat
+++ venv310/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\infop\OneDrive\Desktop\ESP32\ESP32\venv310
+set VIRTUAL_ENV=C:\Users\infop\OneDrive\Desktop\ESP32\pratham-smartplant\venv310
 
 if not defined PROMPT set PROMPT=$P$G
 
